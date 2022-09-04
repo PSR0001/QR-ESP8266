250f9ac609ca1e18523bf12b32399f48542037d3
CoolD69 <parthasingharoy166@gmail.com>
2022-09-04 17:21:33 +0530

    Wifi DNS & Route SetUp

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,67 @@
-#include<Arduino.h>
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
 #include <ESP8266mDNS.h>
 #include <EEPROM.h>
-
 #include <memory>
-//#define FPSTR(pstr_pointer) (reinterpret_cast<const __FlashStringHelper *>(pstr_pointer))
 
-
-/* Set these to your desired softAP credentials. They are not configurable at runtime */
-#ifndef APSSID
-#define APSSID "ESP_ap"
-#define APPSK "12345678"
-#endif
-
-
+// Global variable
 String clientId = "ESP" + String(ESP.getChipId());
 
-const char HTTP_HEADER[] PROGMEM          = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><title>{t}</title>";
-const char HTTP_STYLE[] PROGMEM           = "<style>.c{text-align: center;} div,input{padding:5px;font-size:1em;} input{width:95%;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#1fa3ec;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} .q{float: right;width: 64px;text-align: right;} .l{background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAALVBMVEX///8EBwfBwsLw8PAzNjaCg4NTVVUjJiZDRUUUFxdiZGSho6OSk5Pg4eFydHTCjaf3AAAAZElEQVQ4je2NSw7AIAhEBamKn97/uMXEGBvozkWb9C2Zx4xzWykBhFAeYp9gkLyZE0zIMno9n4g19hmdY39scwqVkOXaxph0ZCXQcqxSpgQpONa59wkRDOL93eAXvimwlbPbwwVAegLS1HGfZAAAAABJRU5ErkJggg==\") no-repeat left center;background-size: 1em;}</style>";
-const char HTTP_SCRIPT[] PROGMEM          = "<script>function c(l){document.getElementById('s').value=l.innerText||l.textContent;document.getElementById('p').focus();}</script>";
-const char HTTP_HEADER_END[] PROGMEM        = "</head><body><div style='text-align:left;display:inline-block;min-width:260px;'>";
-const char HTTP_PORTAL_OPTIONS[] PROGMEM  = "<form action=\"/wifi\" method=\"get\"><button>Configure WiFi</button></form><br/><form action=\"/0wifi\" method=\"get\"><button>Configure WiFi (No Scan)</button></form><br/><form action=\"/i\" method=\"get\"><button>Info</button></form><br/><form action=\"/r\" method=\"post\"><button>Reset</button></form>";
-const char HTTP_ITEM[] PROGMEM            = "<div><a href='#p' onclick='c(this)'>{v}</a>&nbsp;<span class='q {i}'>{r}%</span></div>";
-const char HTTP_FORM_START[] PROGMEM      = "<form method='get' action='wifisave'><input id='s' name='s' length=32 placeholder='SSID'><br/><input id='p' name='p' length=64 type='password' placeholder='password'><br/>";
-const char HTTP_FORM_PARAM[] PROGMEM      = "<br/><input id='{i}' name='{n}' maxlength={l} placeholder='{p}' value='{v}' {c}>";
-const char HTTP_FORM_END[] PROGMEM        = "<br/><button type='submit'>save</button></form>";
-const char HTTP_SCAN_LINK[] PROGMEM       = "<br/><div class=\"c\"><a href=\"/wifi\">Scan</a></div>";
-const char HTTP_SAVED[] PROGMEM           = "<div>Credentials Saved<br />Trying to connect ESP to network.<br />If it fails reconnect to AP to try again</div>";
-const char HTTP_END[] PROGMEM             = "</div></body></html>";
+const char HTTP_HEADER[] PROGMEM = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/><title>{t}</title>";
+const char HTTP_STYLE[] PROGMEM = "<style>body{text-align:center;font-family:verdana;}.a{max-width:350px;margin:auto}button{border:0;border-radius:5px;background-color:#0fe07b;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;}input{padding:5px;font-size:1em;width:95%;}</style>";
+const char HTTP_SCRIPT[] PROGMEM = "";
+const char HTTP_HEADER_END[] PROGMEM = "</head><body><div class=\"a\">";
+const char HTTP_H1[] PROGMEM = "<h2>{h}</h2>";
+const char HTTP_MAIN_PAGE[] PROGMEM = "<hr><br><a href=\"/w\"><button>ConnecttoWiFi</button></a><br><br><a href=\"/q\"><button>ScanQR</button></a>";
+
+const char HTTP_FORM_START[] PROGMEM = "<hr><form method='get'action='/s'><input id='s'name='s'length=32 placeholder='SSID'><br><br><input id='p'name='p'length=64 type='password'placeholder='Password'><br><br><button type='submit'>save</button></form>";
+const char HTTP_QR_START[] PROGMEM = "";
+
+const char HTTP_FORM_PARAM[] PROGMEM = "";
+const char HTTP_FORM_END[] PROGMEM = "";
+const char HTTP_END[] PROGMEM = "</div></body></html>";
+
+// "/W" === WIFI SETUP
+// "/q" === QR CODE
 
 
-
-
-// const char *softAP_ssid = APSSID;
-const char *softAP_password = APPSK;
-
-/* hostname for mDNS. Should work at least on windows. Try http://esp8266.local */
-// const char *myHostname = clientId;
-
-/* Don't set this wifi credentials. They are configurated at runtime and stored on EEPROM */
 char ssid[33] = "";
 char password[65] = "";
 
-// DNS server
-const byte DNS_PORT = 53;
-DNSServer dnsServer;
-
-// Web server
+// ESP Server
 ESP8266WebServer server(80);
 
 /* Soft AP network parameters */
-IPAddress apIP(10,10, 10, 1);
+IPAddress apIP(192, 168, 1, 1);
 IPAddress netMsk(255, 255, 255, 0);
+const byte DNS_PORT = 53;
+DNSServer dnsServer;
 
-
-/** Should I connect to WLAN asap? */
-boolean connect;
-
-/** Last time I tried to connect to WLAN */
-unsigned long lastConnectTry = 0;
-
-/** Current WLAN status */
-unsigned int status = WL_IDLE_STATUS;
-
+// Function Prototype
 void handleRoot();
+void handleWifi();
+String allInfo();
+void serverRoute();
+void dnsStart();
+void handleQr();
+void handleWifiDetails();
 
 
-void setup() {
+
+void setup()
+{
   delay(1000);
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("Configuring access point...");
-  /* You can remove the password parameter if you want the AP to be open. */
-  WiFi.softAPConfig(apIP, apIP, netMsk);
-  WiFi.softAP(clientId, softAP_password);
-  delay(500);  // Without delay I've seen the IP address blank
-  Serial.print("AP IP address: ");
-  Serial.println(WiFi.softAPIP());
-
-  /* Setup the DNS server redirecting all the domains to the apIP */
-  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
-  dnsServer.start(DNS_PORT, "*", apIP);
-
-  /* Setup web pages: root, wifi config pages, SO captive portal detectors and not found. */
-  server.on("/", handleRoot);
-//   server.on("/wifi", handleWifi);
-//   server.on("/wifisave", handleWifiSave);
-  server.on("/generate_204", handleRoot);  // Android captive portal. Maybe not needed. Might be handled by notFound handler.
-  server.on("/fwlink", handleRoot);        // Microsoft captive portal. Maybe not needed. Might be handled by notFound handler.
-//   server.onNotFound(handleNotFound);
-  server.begin();  // Web server start
-  Serial.println("HTTP server started");
- // loadCredentials();           // Load WLAN credentials from network
-  connect = strlen(ssid) > 0;  // Request WLAN connect if there is a SSID
+  dnsStart();
+  serverRoute();
 }
 
-void connectWifi() {
-  Serial.println("Connecting as wifi client...");
+void connectWifi()
+{
+  Serial.println(F("Connecting as wifi client..."));
   WiFi.disconnect();
   WiFi.begin(ssid, password);
   int connRes = WiFi.waitForConnectResult();
@@ -107,46 +69,9 @@ void connectWifi() {
   Serial.println(connRes);
 }
 
-void loop() {
-  if (connect) {
-    Serial.println("Connect requested");
-    connect = false;
-    connectWifi();
-    lastConnectTry = millis();
-  }
-  {
-    unsigned int s = WiFi.status();
-    if (s == 0 && millis() > (lastConnectTry + 60000)) {
-      /* If WLAN disconnected and idle try to connect */
-      /* Don't set retry time too low as retry interfere the softAP operation */
-      connect = true;
-    }
-    if (status != s) {  // WLAN status change
-      Serial.print("Status: ");
-      Serial.println(s);
-      status = s;
-      if (s == WL_CONNECTED) {
-        /* Just connected to WLAN */
-        Serial.println("");
-        Serial.print("Connected to ");
-        Serial.println(ssid);
-        Serial.print("IP address: ");
-        Serial.println(WiFi.localIP());
+void loop()
+{
 
-        // Setup MDNS responder
-        if (!MDNS.begin(clientId)) {
-          Serial.println("Error setting up MDNS responder!");
-        } else {
-          Serial.println("mDNS responder started");
-          // Add service to MDNS-SD
-          MDNS.addService("http", "tcp", 80);
-        }
-      } else if (s == WL_NO_SSID_AVAIL) {
-        WiFi.disconnect();
-      }
-    }
-    if (s == WL_CONNECTED) { MDNS.update(); }
-  }
   // Do work:
   // DNS
   dnsServer.processNextRequest();
@@ -154,33 +79,104 @@ void loop() {
   server.handleClient();
 }
 
+void dnsStart(){
+    Serial.println(F("Configuring access point..."));
 
+  WiFi.softAPConfig(apIP, apIP, netMsk);
+  WiFi.softAP(clientId);
+  delay(500); // !important
 
+  Serial.print(F("AP IP address: "));
+  Serial.println(WiFi.softAPIP());
 
+  // SET TTL time 300 for accessing domain name
+  dnsServer.setTTL(300);
+  // Setup the DNS server redirecting all the domains to the apIP
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
+  dnsServer.start(DNS_PORT, "www.espqr.com", apIP);
+  
+}
 
-void handleRoot() {
+void serverRoute()
+{
+  server.on("/", handleRoot);
+  server.on("/w", handleWifi);
+  server.on("/q", handleQr);
+  server.on("/s", handleWifiDetails);  
+  server.on("/fwlink", handleRoot);        
+  server.onNotFound(handleRoot);
+  server.begin(); // Web server start
+  Serial.println(F("HTTP server started"));
+}
+
+void handleRoot()
+{
   String page = FPSTR(HTTP_HEADER);
-  page.replace("{v}", "Options");
-  page += FPSTR(HTTP_SCRIPT);
+  page.replace("{t}", "WiFi | QR");
   page += FPSTR(HTTP_STYLE);
   page += FPSTR(HTTP_HEADER_END);
-  page += FPSTR(HTTP_PORTAL_OPTIONS);
+  page += FPSTR(HTTP_H1);
+  page.replace("{h}", "QR Code | WiFi");
+  page += FPSTR(HTTP_MAIN_PAGE);
   page += FPSTR(HTTP_END);
 
   server.sendHeader("Content-Length", String(page.length()));
   server.send(200, "text/html", page);
+}
+void handleWifi()
+{
+  String page = FPSTR(HTTP_HEADER);
+  page.replace("{t}", "WiFi");
+  page += FPSTR(HTTP_STYLE);
+  page += FPSTR(HTTP_HEADER_END);
+  page += FPSTR(HTTP_H1);
+  page.replace("{h}", "WiFi 📶");
+  page += FPSTR(HTTP_FORM_START);
+  page += FPSTR(HTTP_END);
+
+  server.sendHeader("Content-Length", String(page.length()));
+  server.send(200, "text/html", page);
+}
+void handleQr()
+{
+  String page = FPSTR(HTTP_HEADER);
+  page.replace("{t}", "QR-Code");
+  page += FPSTR(HTTP_STYLE);
+  page += FPSTR(HTTP_HEADER_END);
+  page += FPSTR(HTTP_H1);
+  page.replace("{h}", "QR-Code");
+  page += FPSTR(HTTP_QR_START);
+  page += FPSTR(HTTP_END);
+
+  server.sendHeader("Content-Length", String(page.length()));
+  server.send(200, "text/html", page);
+}
+
+void handleWifiDetails(){
+
+
+
+
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
+/** Handle the info page */
+String allInfo()
+{
+  String info = "";
+  info += ESP.getChipId();
+  info += "\n";
+  info += ESP.getFlashChipId();
+  info += "\n";
+  info += ESP.getFlashChipSize();
+  info += "\n";
+  info += ESP.getFlashChipRealSize();
+  info += "\n";
+  info += WiFi.softAPIP().toString();
+  info += "\n";
+  info += WiFi.softAPmacAddress();
+  info += "\n";
+  info += WiFi.macAddress();
+  return info;
+}
 
